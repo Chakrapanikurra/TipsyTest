4815d0a90e79f127d86bee59a19034d0351d74b5
Chakrapanikurra <chakra.kurra@gmail.com>
2024-11-22 11:33:47 -0700

    Initial Commit

---
+++ TipsyTest/TipsyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsyTestApp.swift
+//  TipsyTest
+//
+//  Created by Chakrapani Kurra on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct TipsyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
